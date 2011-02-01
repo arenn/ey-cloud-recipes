9eff838016a1d564975711bb39717518f048759b
Aaron M. Renn <arenn@urbanophile.com>
2011-01-31 20:24:44 -0600

    Initial cut at installing ruby-gnome2

---
+++ cookbooks/ruby-gnome2/recipes/default.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: ruby-gnome2
+# Recipe:: default
+#
+
+  ey_cloud_report "ruby-gnome2" do
+    message "processing ruby-gnome2"
+  end
+
+  remote_file "/home/deploy/ruby-gnome2-all-0.90.4.tar.gz" do
+    source "ruby-gnome2-all-0.90.4.tar.gz"
+    owner "deploy"
+    group "deploy"
+    mode 0644
+  end
+
+  remote_file "/home/deploy/install-rubygnome2" do
+    source "install-rubygnome2"
+    owner "deploy"
+    group "deploy"
+    mode 0755
+  end
+
+  execute "install-rubygnome" do
+    command "/home/deploy/install-rubygnome2"
+    action :run
+    user 'deploy'
+  end
+
